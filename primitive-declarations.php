a4428949d0d2bbbbd24b49afdb6e135f7d0d4afe
sebtick <90834370+sebtick@users.noreply.github.com>
2022-11-24 11:02:37 +0100

    primitive-declarations

---
+++ primitive-declarations.php
@@ -0,0 +1,15 @@
+<?php
+$firstStr = "SUUUUUUU";
+$firstEscapeStr = "COUCOU \n";
+$firstBool = true;
+$firstInteger = 13;
+$firstFloat = 15.2;
+$firstNull = null;
+$firstArray = [
+    0 => 2,
+    1 => null,
+    2 => 'Hi',
+    3 => 5.5,
+    4 => false
+];
+?>
